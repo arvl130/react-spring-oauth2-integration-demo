2d3f1746fb3da02e59b164872c732ee12ba8442b
Angelo Geulin <angelogeulin123@gmail.com>
2026-05-30 01:37:16 +0800

    feat: add user id to current user API endpoint

--- oauth2-resource-server/src/main/java/com/ageulin/oauth2_resource_server/controllers/MeController.java
+++ oauth2-resource-server/src/main/java/com/ageulin/oauth2_resource_server/controllers/MeController.java
@@ -1,6 +1,7 @@
 package com.ageulin.oauth2_resource_server.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,14 @@ import java.util.Map;
 @RequestMapping("me")
 public class MeController {
     @GetMapping
-    public ResponseEntity<Map<String, String>> getMe() {
-        final Map<String, String> body = new HashMap<>();
+    public ResponseEntity<Map<String, Object>> getMe(Authentication authentication) {
+        final Map<String, String> data = new HashMap<>();
+        data.put("id", authentication.getName());
+
+        final Map<String, Object> body = new HashMap<>();
+
         body.put("message", "Hello, me!");
+        body.put("data", data);
 
         return ResponseEntity.ok(body);
     }
